0840302761b39caeb9fc0a3a4a3eed63487beefa
siyabendboral <boralsiyabend@gmail.com>
2024-01-26 21:17:21 +0000

    added project id

--- account/main.tf
+++ account/main.tf
@@ -18,3 +18,13 @@ resource "google_project" "testproject" {
 	project_id = random_string.random.result
 	billing_account = data.google_billing_account.acct.id
 }
+
+resource "null_resource" "set-project" {
+	triggers = {
+    always_run = "${timestamp()}"
+  }
+	
+	provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.testproject.project_id}"
+	}
+}
